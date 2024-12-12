050524e057c4f65d73d177d38a4c2a2e40d87594
Victoria Todd <victoria.e.todd.98@gmail.com>
2024-12-12 20:07:33 +0100

    copy across last week's files

---
+++ Week-6/public/js/scripts.js
@@ -0,0 +1,63 @@
+function addCards(data) {
+    data.forEach((item) => {
+        const $card = $(`
+                <div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">${item.firstName} ${item.lastName}</span>
+                            <p>${item.email}</p>
+                        </div>
+                    </div>
+                </div>
+              `);
+
+        $('#card-container').append($card);
+    });
+}
+
+const formSumitted = () => {
+    let formData = {};
+    formData.firstName = $('#first_name').val();
+    formData.lastName = $('#last_name').val();
+    formData.email = $('#email').val();
+
+    console.log(formData);
+    postPerson(formData);
+}
+
+function postPerson(person) {
+    $.ajax({
+        url:'/api/person',
+        type:'POST',
+        data:person,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('person posted');
+                location.reload();
+            }
+        }
+    });
+}
+
+function getAllPeople() {
+    $.get('/api/person',(result)=>{
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    });
+}
+
+let socket = io();
+socket.on('number',(msg)=>{
+    console.log('Random Number: ' + msg);
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#submit-button').click(()=>{
+        formSumitted();
+    });
+    $('.modal').modal();
+    getAllPeople();
+    console.log('ready');
+});
---
+++ Week-6/routers/router.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
+
+router.post('/', function(req,res){
+    controller.postPerson(req,res);
+});
+
+router.get('/', (req,res)=>{
+    controller.getAllPeople(req,res);
+});
+
+router.delete('/', (req,res)=>{
+    controller.getAllPeople(req,res);
+});
+
+
+module.exports = router;
