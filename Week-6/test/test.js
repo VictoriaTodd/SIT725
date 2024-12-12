d9423c03ce2e6504f9e9f9456c2ef19d27f2c6e5
Victoria Todd <victoria.e.todd.98@gmail.com>
2024-12-12 22:19:41 +0100

    delete commented out code

--- Week-6/test/test.js
+++ Week-6/test/test.js
@@ -1,29 +1,4 @@
 const chai = require('chai');
-// const request = require("request");
-// let url = 'http://localhost:3000/api/person';
-// let person = {firstName:'',lastName:'', email: ''}
-
-
-// describe('test GET api', function(){
-//     it('returns statusCode of 200', function(done){
-//         request(url, function(a,b,c){
-//             let responseObj = JSON.parse(c);
-//             expect(responseObj.statusCode).to.equal(200);
-//             done();
-//         });
-//     });
-// });
-
-// describe('test POST api', function(){
-//     it('post person to DB', function(done){
-//         request.post({url:url,form:person}, function(a,b,c){
-//             let responseObj = JSON.parse(c);
-//             expect(responseObj.statusCode).to.equal(201);
-//             done();
-//         });
-//     });
-// });
-
 const sinon = require('sinon');
 const { expect } = chai;
 
